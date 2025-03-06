724ee4c51064c2c6615e46e645e41430448157ab
tomejunior <alexandretomejunior@outlook.com>
2025-03-06 12:43:25 -0300

    Create SuperTrunfoPT2.c

---
+++ SuperTrunfoPT2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+     int main(){
+       
+        char estado[20];
+        char cidade[20];
+        char codigo[20];
+        int populacao;
+        float area = 693.442;
+        float pib = 23.90793;
+        int turisticos;
+         
+
+        if ( pib >21.706,06){
+           printf("Carta1 - Salvador(BA):21.706,06\n");
+           } else{
+           printf("Carta2-Vitória da Conquista (BA):23.907,93\n");
+           }
+        
+           if ( area < 693.442){
+           printf("Carta1 - Salvador(BA):693,442\n");
+           } else {
+           printf("Carta2-Vitória da Conquista (BA):3.254,186\n");
+           }
+return 0;
+     }
